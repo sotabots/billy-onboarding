77fc7fce7c93b513c740515b445dd8888b6e5f3a
Anton Kostin <masguit42@gmail.com>
2024-06-24 13:20:45 +0800

    another iteration

--- app/src/config.ts
+++ app/src/config.ts
@@ -13,13 +13,13 @@ export default defineConfig({
           media: {
             type: 'image',
             src: import('./assets/img/logo_koala_no_background.png'),
-            size: 250,
+            size: 150,
           },
           shape: 'rounded',
           pagination: 'count',
           title: 'Привет, это Билли',
-          description: "Я бот для ведения общих расходов, записи групповых трат и финального расчёта друг с другом в путешествии, в сожительстве, в паре.",
-          button: 'ДА КТО ТАКОЙ ВАШ ЭТОТ БИЛЛИ?! ',
+          description: "Я бот для ведения общих расходов, записи групповых трат и финального расчёта друг с другом в путешествии, в компании, в паре.",
+          button: 'ДА КТО ТАКОЙ ЭТОТ ВАШ БИЛЛИ?!',
         },
 
         // 2
@@ -27,13 +27,13 @@ export default defineConfig({
           media: {
             type: 'image',
             src: import('./assets/img/buble_message.webp'),
-            size: 250,
+            size: 150,
           },
           shape: 'square',
           pagination: 'count',
           title: 'Революционная запись трат',
           description:
-            'Меня достаточно добавить в ваш чат с друзьями. Траты можно записывать как текстом, так и голосовыми сообщениями! Я найду эти сообщения в чате и всё учту.',
+            'Просто добавь меня в ваш чат, чтобы начать пользоваться ботом. Траты можно записывать как текстом, так и голосовыми сообщениями! Я найду эти сообщения в чате и всё учту.',
           button: 'ТОП-3 проблемы группового расчёта',
         },
 
@@ -42,14 +42,14 @@ export default defineConfig({
           media: {
             type: 'sticker',
             src: import('./assets/stickers/utya_count_ducks.tgs'),
-            size: 250,
+            size: 150,
           },
           shape: 'square',
           pagination: 'count',
           title: 'Надоело переводить друг другу деньги после каждой общей оплаты и напоминать об этом?',
           description:
             'Достаточно, чтобы кто-то один написал о трате в чат – бот это увидит и запишет в историю. В конце периода я просуммирую все траты и скажу, кто кому сколько должен.',
-          button: 'Проблема 2',
+          button: 'Согласны?',
         },
 
         // 4
@@ -57,14 +57,14 @@ export default defineConfig({
           media: {
             type: 'image',
             src: import('./assets/img/cashback.webp'),
-            size: 250,
+            size: 150,
           },
           shape: 'square',
           pagination: 'count',
           title: 'Никто не хочет брать ответственность за оплату общих расходов?',
           description:
             'Плати за друзей - получай кэшбэк в Билли реальными деньгами!',
-          button: 'Проблема 3',
+          button: 'Узнали?',
         },
 
         // 5
@@ -72,7 +72,7 @@ export default defineConfig({
           media: {
             type: 'image',
             src: import('./assets/img/late_calculate.webp'),
-            size: 250,
+            size: 150,
           },
           shape: 'square',
           pagination: 'count',
@@ -87,75 +87,17 @@ export default defineConfig({
           media: {
             type: 'image',
             src: import('./assets/img/late_calculate.webp'),
-            size: 250,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Билли сделает ведение группового бюджета максимально удобным!',
-          description:
-            'Ну, и на последок:\n- Конвертация валют: упростит финальные расчеты, пересчитав все траты в выбранную валюту. Особенно актуально в путешествиях!\n- Категории: Билли автоматически определит и проставит категорию для каждой траты, а в конце предоставит анализ самых больших из них.',
-          button: 'Попробовать записать трату - Мне всё понятно!',
-        },
-
-        // "everything is customizable" slide
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_xray.tgs'),
-            size: 250,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Everything is customizable',
-          description: '',
-          textAlign: 'center',
-          list: [
-            '<b>CSS styles</b>: extend primary colors from Telegram or set yours',
-            'Button text and actions (look down)',
-            'Use our carefully crafted <b>presets</b> or easily create your own',
-          ],
-          button: 'Super-Duper Next',
-        },
-
-        // slide with other features
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_cool.tgs'),
             size: 150,
           },
           shape: 'square',
           pagination: 'count',
-          title: 'Some other features:',
+          title: 'Самое удобное ведение группового бюджета!',
           description: '',
           list: [
-            'One-click 0$ <b>deploy</b> on GitHub Pages',
-            'Language and currency localization',
-            'Buttons with <b>haptic</b> feedback',
-            'Content pre-loading for high speed',
-            '<b>Low-code</b> approach to building onboardings',
-            'Many examples/presets',
-            "And many more... (see <a href='https://github.com/Easterok/telegram-onboarding-kit' target='_blank'>GitHub</a>)",
+            'Конвертация валют: <br>упростит финальные расчёты, пересчитав все траты в выбранную валюту. Особенно актуально в путешествиях!',
+            'Категории: Билли автоматически определит и проставит категорию для каждой траты, а в конце предоставит анализ самых больших из них.',
           ],
-          button: 'Next',
-        },
-
-        // go to paywall slide
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_knife.tgs'),
-            size: 250,
-          },
-          shape: 'square',
-          pagination: 'count',
-          textAlign: 'center',
-          title: 'But onboarding slides are not enough...',
-          description: "Let's go to Paywall",
-          button: {
-            content: 'Go to Paywall',
-            to: '/paywall',
-          },
+          button: 'Попробовать записать трату',
         },
       ],
     },
