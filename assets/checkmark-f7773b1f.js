dae946fffa16516c8966cbb661a76ab39209feee
Anton Kostin <masguit42@gmail.com>
2024-06-24 20:20:04 +0800

    Updates

--- assets/checkmark-f7773b1f.js
+++
@@ -1,1 +0,0 @@
-import{o as e,e as o,f as t}from"./index-157dc747.js";const r={viewBox:"0 0 28 28",fill:"none",xmlns:"http://www.w3.org/2000/svg"},n=t("path",{d:"M6 15L11.5 20.5L22 9",stroke:"currentColor","stroke-width":"2","stroke-linecap":"round","stroke-linejoin":"round"},null,-1),s=[n];function c(a,l){return e(),o("svg",r,s)}const i={render:c};export{i as default,c as render};
