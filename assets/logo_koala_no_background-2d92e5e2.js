5f36efb58adf2c7aee21606afc3b53e8a1626da7
Anton Kostin <masguit42@gmail.com>
2024-06-24 18:51:51 +0800

    Updates

---
+++ assets/logo_koala_no_background-2d92e5e2.js
@@ -0,0 +1,1 @@
+const o="/billy-onboarding/assets/logo_koala_no_background-1f69ba65.png";export{o as default};
--- assets/logo_koala_no_background-865bdf67.js
+++
@@ -1,1 +0,0 @@
-const o="/billy-onboarding/assets/logo_koala_no_background-d985351c.png";export{o as default};
