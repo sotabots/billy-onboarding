835c1b86be89757e5086bc5a40ef97f625b8271c
iower <iower@tutanota.com>
2024-06-25 17:14:01 +0500

    Try to send data

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,3 +4,16 @@ import App from './App.vue';
 import { default as config } from './config';
 
 bootstrap(App, config);
+
+// @ts-expect-error ...
+window.navigation.addEventListener('navigate', (event) => {
+  console.log('location changed!', event);
+
+  if (event.destination?.url?.includes('finish')) {
+    console.log('send data');
+    // @ts-expect-error ...
+    window.Telegram?.WebApp?.send('finish');
+    // @ts-expect-error ...
+    window.Telegram?.WebApp?.close();
+  }
+});
